73bd383c31857d522a30712a9aecfa4e6f9e43e9
AnilEnsari <anilensari@gmail.com>
2023-07-25 22:02:16 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
-
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
 // Kodlar buraya gelecek!
+document.querySelector(".intro img").addEventListener("mouseover", (e) => {
+  document
+    .querySelector(".intro img")
+    .setAttribute(
+      "src",
+      "https://hlkiurt3.rocketcdn.com/news/bd3b79729bfb4298bc913bbe9cf23b83.jpeg"
+    );
+});
+document.querySelector(".intro img").addEventListener("click", (e) => {
+  document
+    .querySelector(".intro img")
+    .setAttribute("src", "http://localhost:9000/img/fun-bus.jpg");
+});
+const altBaslik = document.querySelector("h2");
+altBaslik.addEventListener("dblclick", (e) => {
+  altBaslik.innerHTML = "ZAAHAHAHAHA";
+});
+
+document.body.addEventListener("keydown", (e) => {
+  document.querySelector("h1").innerHTML = "ŞAMPİYON GALATASARAY";
+});
+document.body.addEventListener("scroll", (e) => {
+  document.querySelector("nav a").innerHTML = "MAURO";
+});
